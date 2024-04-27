53285bc61af1bcae689dd3039440eec462e39299
unknown <afyaakhudur@gmail.com>
2024-04-27 19:25:03 +0300

    complete assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [availability , setAvailability]=useState(true)
+  const listClass = availability? "" : "in-cart"
+
+  function addToCart(){
+    setAvailability( !availability)
+  }
   return (
-    <li className="">
+    <li className={listClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addToCart}>{availability ? 'Add to Cart' : 'Remove From Cart' }</button>
     </li>
   );
 }
